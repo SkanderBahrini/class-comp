133d61ee8abf21ceaf46bc15378f8af9cc9cfed1
Skander Bahrini <bahriniskander79@gmail.com>
2021-08-11 12:50:11 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
+class App extends React.Component {
+   state ={
+
+     fullname:"messi",
+     bio:"football",
+     professsion:"barca",
+     imgsrc:"",
+
+     boul:false
+
+    };
+       handleclick=()=>{
+         this.setState({boul : true})
+   this.setState({bio:"khadem"})}
+
+ 
+  render(){
+
+      return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+   {  this.state.boul && <div> {this.state.bio} and {this.state.profession} and { this.state.imgsrc} </div> }
+      
+     <buton onClick={this.handleclick()}> click me {setInterval(alert('change time'))} </buton>
+
     </div>
   );
+  }
+  
+      
+   
+  
 }
 
 export default App;
